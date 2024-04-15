aa49efda81a2296b4fed345398f51ebdb27fe10f
Deba2607 <debadityo2607@gmail.com>
2024-04-15 17:59:56 +0530

    edit 1

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,14 @@
 import "package:flutter/material.dart";
 import "package:flutter_catalog/utils/routes.dart";
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -18,7 +23,7 @@ class LoginPage extends StatelessWidget {
               height: 20.0,
             ),
             const Text(
-              "Welcome",
+              "Welcome ",
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             const SizedBox(
